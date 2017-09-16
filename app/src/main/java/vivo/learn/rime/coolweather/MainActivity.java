821b8f8384c131baf47932875b8e1d00789436fe
RimeT <dacong_54321@163.com>
2017-09-16 11:14:02 +0800

    Make Cool Weather Project. add Litepal Glide Gson okhttp libs

---
+++ app/src/main/java/vivo/learn/rime/coolweather/MainActivity.java
@@ -0,0 +1,18 @@
+package vivo.learn.rime.coolweather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by RimeT on 2017/9/16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/vivo/learn/rime/coolweather/db/Province.java
@@ -0,0 +1,41 @@
+package vivo.learn.rime.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by RimeT on 2017/9/16.
+ * This is a data struct store info of province
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
